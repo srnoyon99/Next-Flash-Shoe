f32289f917414e8cdc35b3de47d4e1cc721aa20a
5599noyon <shahriarnoyon5599@gmail.com>
2026-09-07 11:16:30 +0600

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import WhatsApp from "@/components/whatsapp";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-full flex flex-col">
         <Navbar/>
         {children}
+        <WhatsApp/>
         <Footer/>
       </body>
     </html>
